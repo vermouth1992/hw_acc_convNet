fbeb17f967872222336ae5489816235da9d13cd8
vermouth1992 <czhangseu@gmail.com>
2016-07-06 12:20:32 -0700

    start twoDConv

--- my-first-chisel/src/main/scala/two_d_convolution.scala
+++ my-first-chisel/src/main/scala/two_d_convolution.scala
@@ -14,4 +14,25 @@ class two_d_convolution(DATA_WIDTH: Int, k: Int, M: Int) extends Module {
   }
 
   // instantiate fft module, all together k fft
+  val fftArrayFirst = Vec.fill(k) { Module(new fft(DATA_WIDTH, M, M)).io }
+  for (i <- 0 until k) {
+    fftArrayFirst(i).next := io.start
+    for (j <- 0 until M) {
+      fftArrayFirst(i).input(2 * j) := io.in(i * M + j)
+      fftArrayFirst(i).input(2 * j + 1) := UInt(0)
+    }
+  }
+
+  // define the first matrixTranspose
+  val matrixTransposeFirst = Module(new streamMatrixTransposeTop(2 * DATA_WIDTH, k, M)).io
+  matrixTransposeFirst.start := fftArrayFirst(0).next_out
+
+  for (i <- 0 until k) {
+    for (j <- 0 until M) {
+      matrixTransposeFirst.in(i * M + j) := Cat(fftArrayFirst(i).input(2 * j + 1), fftArrayFirst(i).input(2 * j))
+    }
+  }
+
+  //
+
 }
