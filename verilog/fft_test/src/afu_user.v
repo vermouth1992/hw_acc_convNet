2b5877b127b04325c253bdb3f3c1fbc526e91911
vermouth1992 <czhangseu@gmail.com>
2016-07-22 16:26:30 -0700

    modify afu_user.v

--- verilog/fft_test/src/afu_user.v
+++ verilog/fft_test/src/afu_user.v
@@ -24,10 +24,7 @@ module afu_user # (
   wire input_fifo_empty;
   // output fifo
   wire [511:0] output_fifo_din;
-  // uut
-  reg start;
-  wire clk_en;
-  wire start_next_stage;
+  reg output_fifo_we;
 
   syn_read_fifo #(.FIFO_WIDTH(512),
                   .FIFO_DEPTH_BITS(BUFF_DEPTH_BITS),       // transfer size 1 -> 32 entries
@@ -47,167 +44,26 @@ module afu_user # (
                 .almostfull         (input_fifo_almost_full)
             );
 
-  wire [DATA_WIDTH-1:0] in0;
-  wire [DATA_WIDTH-1:0] in1;
-  wire [DATA_WIDTH-1:0] in2;
-  wire [DATA_WIDTH-1:0] in3;
-  wire [DATA_WIDTH-1:0] in4;
-  wire [DATA_WIDTH-1:0] in5;
-  wire [DATA_WIDTH-1:0] in6;
-  wire [DATA_WIDTH-1:0] in7;
-  wire [DATA_WIDTH-1:0] in8;
-  wire [DATA_WIDTH-1:0] in9;
-  wire [DATA_WIDTH-1:0] in10;
-  wire [DATA_WIDTH-1:0] in11;
-  wire [DATA_WIDTH-1:0] in12;
-  wire [DATA_WIDTH-1:0] in13;
-  wire [DATA_WIDTH-1:0] in14;
-  wire [DATA_WIDTH-1:0] in15;
+  intf_fft fft_io(clk, reset);
 
-  wire [DATA_WIDTH-1:0] out0;
-  wire [DATA_WIDTH-1:0] out1;
-  wire [DATA_WIDTH-1:0] out2;
-  wire [DATA_WIDTH-1:0] out3;
-  wire [DATA_WIDTH-1:0] out4;
-  wire [DATA_WIDTH-1:0] out5;
-  wire [DATA_WIDTH-1:0] out6;
-  wire [DATA_WIDTH-1:0] out7;
-  wire [DATA_WIDTH-1:0] out8;
-  wire [DATA_WIDTH-1:0] out9;
-  wire [DATA_WIDTH-1:0] out10;
-  wire [DATA_WIDTH-1:0] out11;
-  wire [DATA_WIDTH-1:0] out12;
-  wire [DATA_WIDTH-1:0] out13;
-  wire [DATA_WIDTH-1:0] out14;
-  wire [DATA_WIDTH-1:0] out15;
+  fft_wrapper fft_inst(fft_io);
 
-  assign in0 = input_fifo_dout[31:0];
-  assign in1 = input_fifo_dout[63:32];
-  assign in2 = input_fifo_dout[95:64];
-  assign in3 = input_fifo_dout[127:96];
-  assign in4 = input_fifo_dout[159:128];
-  assign in5 = input_fifo_dout[191:160];
-  assign in6 = input_fifo_dout[223:192];
-  assign in7 = input_fifo_dout[255:224];
-  assign in8 = input_fifo_dout[287:256];
-  assign in9 = input_fifo_dout[319:288];
-  assign in10 = input_fifo_dout[351:320];
-  assign in11 = input_fifo_dout[383:352];
-  assign in12 = input_fifo_dout[415:384];
-  assign in13 = input_fifo_dout[447:416];
-  assign in14 = input_fifo_dout[479:448];
-  assign in15 = input_fifo_dout[511:480];
-
-  assign input_fifo_re = (reset == 1'b1) ? 1'b0 : ~input_fifo_empty;
-
-  always @(posedge clk) begin
-    if (reset) begin
-      start <= 1'b0;
-    end else if (input_fifo_re == 1'b1) begin
-      start <= 1'b1;
-    end else begin
-      start <= 1'b0;
+  genvar i;
+  generate
+    for (i=0; i<16; i=i+1) begin: fft_input_generate
+      assign fft_io.in[i] = input_fifo_dout[32*i+31:32*i];
+      assign output_fifo_din[32*i+31:32*i] = fft_io.out[i];
     end
-  end
+  endgenerate
 
-  reg [31:0] ctx_input_count;
+  // input re
+  assign input_fifo_re = ~input_fifo_empty;
+  // io next
+  assign fft_io.next = input_fifo_re;
 
   always@(posedge clk) begin
-    if (reset) begin
-      ctx_input_count <= 0;
-    end else if (input_fifo_re) begin
-      ctx_input_count <= ctx_input_count + 1'b1;
-    end
+    output_fifo_we <= fft_io.next_out;
   end
-
-  assign clk_en = (ctx_input_count == ctx_length) ? 1'b1 : start;
-
-  streamMatrixTransposeTop # (
-    .DATA_WIDTH(DATA_WIDTH)
-    ) uut (
-    .clk(clk),
-    .clk_en(clk_en),
-    .start(start),
-    .reset(reset),
-    .in0(in0),
-    .in1(in1),
-    .in2(in2),
-    .in3(in3),
-    .in4(in4),
-    .in5(in5),
-    .in6(in6),
-    .in7(in7),
-    .in8(in8),
-    .in9(in9),
-    .in10(in10),
-    .in11(in11),
-    .in12(in12),
-    .in13(in13),
-    .in14(in14),
-    .in15(in15),
-    .start_next_stage(start_next_stage),
-    .out0(out0),
-    .out1(out1),
-    .out2(out2),
-    .out3(out3),
-    .out4(out4),
-    .out5(out5),
-    .out6(out6),
-    .out7(out7),
-    .out8(out8),
-    .out9(out9),
-    .out10(out10),
-    .out11(out11),
-    .out12(out12),
-    .out13(out13),
-    .out14(out14),
-    .out15(out15)
-  );
-
-  wire output_fifo_we;
-
-  reg [31:0] ctx_output_count;
-
-  always@(posedge clk) begin
-    if (reset) begin
-      ctx_output_count <= 0;
-    end else if (output_fifo_we) begin
-      ctx_output_count <= ctx_output_count + 1'b1;
-    end
-  end
-
-  assign output_fifo_we = (ctx_length == ctx_output_count) ? 1'b0 : start_next_stage & clk_en;
-
-  // total_time_count
-  reg [31:0] total_time_count;
-  reg total_time_count_state;
-  localparam IDLE = 1'b0;
-  localparam RUN = 1'b1;
-  always @(posedge clk) begin : proc_total_time_count
-    if(reset) begin
-      total_time_count <= 0;
-      total_time_count_state <= IDLE;
-    end else begin
-      case (total_time_count_state)
-        IDLE: begin
-          if (output_fifo_we) begin
-            total_time_count_state <= RUN;
-          end
-        end
-        RUN: begin
-          if (ctx_length != ctx_output_count) begin
-            total_time_count <= total_time_count + 1'b1;
-          end
-        end
-      endcase // total_time_count_state
-    end
-  end
-
-  wire [511:0] uut_dout = {out15, out14, out13, out12, out11, out10, out9, out8, 
-                            out7, out6, out5, out4, out3, out2, out1, out0
-                            };
-
-  assign output_fifo_din = (ctx_length - 1 == ctx_output_count) ? {480'b0, total_time_count} : uut_dout;
 
   syn_read_fifo #(.FIFO_WIDTH(512),
                   .FIFO_DEPTH_BITS(BUFF_DEPTH_BITS),       // transfer size 1 -> 32 entries
