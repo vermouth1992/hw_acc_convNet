6324f3302841a938875580a010b827f55d7209d7
vermouth1992 <czhangseu@gmail.com>
2016-07-23 23:57:21 -0700

    write a Matlab version of vgg-16

---
+++ python-scripts/convNets/common.py
@@ -0,0 +1,2 @@
+
+data_set_root = "/Users/chizhang/documents/matlab/usc/fpga2017/data_sets/"
--- python-scripts/convNets/vgg-16_keras.py
+++ python-scripts/convNets/vgg-16_keras.py
@@ -4,50 +4,52 @@ from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2
 from keras.optimizers import SGD
 import cv2, numpy as np
 
+from common import *
+
 
 def VGG_16(weights_path=None):
     model = Sequential()
-    model.add(ZeroPadding2D((1, 1), input_shape=(3, 224, 224)))
+    model.add(ZeroPadding2D((1, 1), input_shape=(3, 224, 224)))   # layer_0
     model.add(Convolution2D(64, 3, 3, activation='relu'))
     model.add(ZeroPadding2D((1, 1)))
     model.add(Convolution2D(64, 3, 3, activation='relu'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-    model.add(ZeroPadding2D((1, 1)))
+    model.add(ZeroPadding2D((1, 1)))                         # layer_5
     model.add(Convolution2D(128, 3, 3, activation='relu'))
     model.add(ZeroPadding2D((1, 1)))
     model.add(Convolution2D(128, 3, 3, activation='relu'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-    model.add(ZeroPadding2D((1, 1)))
+    model.add(ZeroPadding2D((1, 1)))                         # layer_10
     model.add(Convolution2D(256, 3, 3, activation='relu'))
     model.add(ZeroPadding2D((1, 1)))
     model.add(Convolution2D(256, 3, 3, activation='relu'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))   # layer_15
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(ZeroPadding2D((1, 1)))                         # layer_17
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))   # layer_20
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))   # layer_25
     model.add(ZeroPadding2D((1, 1)))
     model.add(Convolution2D(512, 3, 3, activation='relu'))
     model.add(ZeroPadding2D((1, 1)))
     model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))          # layer_30
 
     model.add(Flatten())
     model.add(Dense(4096, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(4096, activation='relu'))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.5))                                  # layer_35
     model.add(Dense(1000, activation='softmax'))
 
     if weights_path:
@@ -65,7 +67,7 @@ def getCategory(index):
 
 
 if __name__ == "__main__":
-    im = cv2.resize(cv2.imread('goldfish.jpeg'), (224, 224)).astype(np.float32)
+    im = cv2.resize(cv2.imread(data_set_root + 'goldfish.jpeg'), (224, 224)).astype(np.float32)
     im[:, :, 0] -= 103.939
     im[:, :, 1] -= 116.779
     im[:, :, 2] -= 123.68
@@ -73,9 +75,10 @@ if __name__ == "__main__":
     im = np.expand_dims(im, axis=0)
 
     # Test pre-trained model
-    model = VGG_16('vgg16_weights.h5')
+    model = VGG_16(data_set_root + 'vgg16_weights.h5')
     sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(optimizer=sgd, loss='categorical_crossentropy')
     out = model.predict(im)
+    print out
     index = np.argmax(out)
     print index, getCategory(index)
