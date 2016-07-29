ea9672832a1596cd2ded406a6aa9262568a6271d
Chi Zhang <zhan527@usc.edu>
2016-07-29 18:57:02 -0400

    generate synthesis env py

--- python-scripts/util/generate_synthesis_environment.py
+++ python-scripts/util/generate_synthesis_environment.py
@@ -4,24 +4,27 @@ import os
 import sys
 
 def add_setting(root, file):
-    file_extension = os.path.splitext(file)
-    filename = root + "/" + file
-    if extension == ".v":
+    file_extension = os.path.splitext(file)[1]
+    filename = root + file
+    if file_extension == ".v":
         tag = "VERILOG_FILE"
-    elif extension == ".sv":
+    elif file_extension == ".sv":
         tag = "SYSTEMVERILOG_FILE"
     else:
         tag = None
 
     if tag != None:
         return "set_global_assignment -name " + tag + \
-            "$MY_WORKDIR/" + filename + "\n"
+            " $MY_WORKDIR/" + filename + "\n"
 
 if __name__ == "__main__":
     # traverse root directory, and list directories as dirs and files as files
     settings = ""
-    for root, dirs, files in os.walk("."):
+    folder = sys.argv[1]
+    for root, dirs, files in os.walk(folder):
         # print root, dirs, files
         for file in files:
-            settings += add_setting(root, file)
+            s = add_setting(root, file)
+            if s != None:
+                settings += s
     print settings
