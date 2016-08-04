ecff7b317c67d0c0db0fa86bb0c023a186db44b9
Chi Zhang <czhangseu@gmail.com>
2016-08-04 12:42:08 +0800

    define bandwidth testing mode

--- app/benchmark/config.h
+++ app/benchmark/config.h
@@ -12,6 +12,10 @@
 // #define  HWAFU
 #define  ASEAFU
 
+#define read_only 1
+#define write_only 2
+#define read_write 3
+
 // convLayer configuration
 #ifdef HWAFU
 #define num_cacheline 10000
@@ -23,5 +27,6 @@
 #endif
 
 #define bufferSize CL(1) * num_cacheline
+#define mode read_only
 
 #endif //SW_APP_CONFIG_H
