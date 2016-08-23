7f4dc2466fa697abf77f962e4547685e56939fb5
vermouth1992 <czhangseu@gmail.com>
2016-08-23 14:06:59 -0700

    finish convLayer data path

--- verilog/convLayer/src/afu_user/convLayerFFT.sv
+++ verilog/convLayer/src/afu_user/convLayerFFT.sv
@@ -9,9 +9,10 @@
 module convLayerFFT (
   input clk,    // Clock
   input reset,  // Synchronous reset active high
-  // data
+  // control
   input next,   // the valid data is on the next cycle
   output next_out,
+  // data
   input [511:0] cacheline_in,  // cache line data
   output complex_t out [0:3][0:3][0:3]
 );
@@ -22,7 +23,7 @@ module convLayerFFT (
   genvar i, j, k;
   generate
     for (i=0; i<4; i=i+1) begin: fft4_2d_inst_gen
-      fft_2d #(4) fft4_2d_inst(fft4_2d_io[i]);
+      fft_2d #(4, 0) fft4_2d_inst(fft4_2d_io[i]);
     end
   endgenerate
 
@@ -77,4 +78,116 @@ module convLayerFFT (
     end
   endgenerate
   
+endmodule
+
+/* 4 ifft_2d instances in parallel */ 
+module convLayerIFFT (
+  input clk,
+  input reset,
+  // control
+  input next,
+  output output_valid,
+  // data
+  input complex_t in [0:3][0:3][0:3],
+  output [511:0] cacheline_out   // the output will be in consecutive 4 cycles
+  );
+
+  complex_t out [0:3][0:3][0:3];
+  wire next_out;
+
+  // 4 fft_2d_io
+  intf_fft_2d #(4) fft4_2d_io[0:3](clk, reset);
+  // instantiate 4 fft_2d
+  genvar i, j, k;
+  generate
+    for (i=0; i<4; i=i+1) begin: fft4_2d_inst_gen
+      fft_2d #(4, 1) fft4_2d_inst(fft4_2d_io[i]);
+    end
+  endgenerate
+
+  // assign input signal
+  generate
+    for (i=0; i<4; i=i+1) begin: in_i
+      assign fft4_2d_io[i].in = in[i];
+    end
+  endgenerate
+
+  // assign output
+  generate
+    for (i=0; i<4; i=i+1) begin: out_i
+      assign out[i] = fft4_2d_io[i].out;
+    end
+  endgenerate
+
+  // assgin next signal
+  generate
+    for (i=0; i<4; i=i+1) begin: next_io
+      assign fft4_2d_io[i].next = next;
+    end
+  endgenerate
+
+  // if it is not correct, then use AND gate
+  assign next_out = fft4_2d_io[0].next_out;
+
+  // select the real part
+  logic [31:0] out_real [0:3][0:3][0:3];
+
+  generate
+    for (i=0; i<4; i=i+1) begin: select_real_outer
+      for (j=0; j<4; j=j+1) begin: select_real_middle
+        for (k=0; k<4; k=k+1) begin: select_real_inner
+          assign out_real[i][j][k] = out[i][j][k].r;
+        end
+      end
+    end
+  endgenerate
+
+  // reformat to 4 cachelines
+  logic [511:0] out_wire [0:3];
+
+  generate
+    for (i=0; i<4; i=i+1) begin: cacheline_index
+      for (j=0; j<4; j=j+1) begin: reformat_outer
+        for (k=0; k<4; k=k+1) begin: reformat_inner
+          assign out_wire[i][128*k+32*k+31:128*j+32*k] = out_real[i][j][k];
+        end
+      end
+    end
+  endgenerate
+
+  // 4 registers, not mem
+  reg [511:0] out_reg [0:3];
+
+  integer m;
+
+  // register the output when next_out is high
+  always@(posedge clk) begin
+    if (next_out) begin
+      for (m=0; m<4; m=m+1) begin
+        out_reg[m] <= out_wire[m];
+      end
+    end
+  end
+
+  // control signal
+  reg [2:0] counter;
+
+  always@(posedge clk) begin
+    if (reset) begin
+      counter <= 3'b100;
+    end else if (next_out) begin
+      counter <= 3'b000;
+    end else if (counter < 3'b100) begin
+      counter <= counter + 1;
+    end
+  end
+
+  assign output_valid = ~counter[2];   // if counter[2] == 1, then not valid, else valid
+
+  // cacheline out based on counter
+  assign cacheline_out = (counter[1:0] == 2'b00) ? out_reg[0] :
+                         (counter[1:0] == 2'b01) ? out_reg[1] :
+                         (counter[1:0] == 2'b10) ? out_reg[2] :
+                         out_reg[3];
+
 endmodule
