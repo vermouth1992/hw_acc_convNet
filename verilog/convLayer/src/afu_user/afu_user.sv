1cff9b114e65d5d4591bcde3eef6d14d674a3141
vermouth1992 <czhangseu@gmail.com>
2016-08-24 20:57:32 -0700

    afu_user FSM

--- verilog/convLayer/src/afu_user/afu_user.sv
+++ verilog/convLayer/src/afu_user/afu_user.sv
@@ -240,23 +240,23 @@ module afu_user #(ADDR_LMT = 20, MDATA = 14, CACHE_WIDTH = 512) (
     .next         (next_ifft),
     .output_valid (output_valid_ifft),
     .in           (in_ifft),
-    .cacheline_out(cacheline_out_ifft),
+    .cacheline_out(cacheline_out_ifft)
     );
 
 
   /********* AFU USER FSM **************/
 
   // state for memory request, currently, it is a image oriented approach
-  enum {TX_RD_STATE_IDLE, TX_RD_STATE_CONFIG, TX_RD_STATE_IMAGE, TX_RD_STATE_KERNEL, TX_RD_STATE_DONE} read_req_state;
+  enum {TX_RD_STATE_IDLE, TX_RD_STATE_IMAGE, TX_RD_STATE_KERNEL_0, TX_RD_STATE_KERNEL_1, TX_RD_STATE_DONE} read_req_state;
 
   // afu_context info extraction
   reg [ADDR_LMT-1:0] filter_offset_addr;
   reg [31:0] num_input_feature_maps, num_output_feature_maps;
-  reg [ADDR_LMT-1:0] current_read_image_addr, current_read_filter_addr;  // read address
-  reg [31:0] num_cl_image, num_cl_image_to_read;
-  reg [31:0] num_cl_filter, num_cl_filter_to_read;
-  // currently, we don't need meta data to communicate
-  // reg [MDATA-1:0] current_read_image_mdata, current_read_filter_mdata;   // representing cache line index
+  reg [ADDR_LMT-1:0] current_read_image_addr, current_read_filter_addr;  // read address from shared memory
+  reg [31:0] num_cl_output_buffer;
+  reg [31:0] num_cl_filter;
+  
+  reg [31:0] current_cycle_already_read_cl_image;
   // read request FSM
   always@(posedge clk) begin
     if (reset) begin
@@ -270,63 +270,50 @@ module afu_user #(ADDR_LMT = 20, MDATA = 14, CACHE_WIDTH = 512) (
             // synthesis translate_off
             $display("src = %h", afu_context[127:64]);
             $display("dest = %h", afu_context[191:128]);
-            $display("num_cl_image = %d", afu_context[223:192]);  // in order to track the filter read
+            $display("num of cacheline in output buffer = %d", afu_context[223:192]);  // in order to track the filter read
             $display("filter offset address = %h", afu_context[256+64-1:256]);   // filter address # of cacheline
-            $display("num input feature map = %d", afu_context[351:320]);   // D1
+            $display("num input feature map = %d", afu_context[351:320]);   // D1, used for accumulate
             $display("num output feature map = %d", afu_context[415:384]);  // D2
             $display("num of cacheline in filter region = %d", afu_context[479:448]);
             // synthesis translate_on
             filter_offset_addr <= afu_context[256+ADDR_LMT-1+6:256+6];    // has to be cacheline aligned
             num_input_feature_maps <= afu_context[351:320];
             num_output_feature_maps <= afu_context[415:384];
-            num_cl_image <= afu_context[223:192];
+            num_cl_output_buffer <= afu_context[223:192];
             num_cl_filter <= afu_context[479:448];
             read_req_state <= TX_RD_STATE_CONFIG;
+            current_cycle_already_read_cl_image <= 0;
           end
-        end
-        TX_RD_STATE_CONFIG: begin
-          // if total number of image in cl is smaller or equal to 8192
-          if (num_cl_image <= NUM_CACHELINE_IMAGE_MOST) begin
-            num_cl_image_to_read <= num_cl_image;
-          end else begin
-            num_cl_image_to_read <= NUM_CACHELINE_IMAGE_MOST;
-          end
-          num_cl_filter_to_read <= num_cl_filter;
-          current_read_filter_addr <= filter_offset_addr;
-          read_req_state <= TX_RD_STATE_IMAGE;
         end
         TX_RD_STATE_IMAGE: begin
-          if (~rd_req_almostfull && num_cl_image_to_read != 0) begin
-            rd_req_addr <= current_read_image_addr;
-            current_read_image_addr <= current_read_image_addr + 1;
-            rd_req_en <= 1'b1;
-            rd_rsp_mdata[0] <= 1'b0;   // 0 represents image
-            // reduce the current cl num to read
-            num_cl_image_to_read <= num_cl_image_to_read - 1;
-            // reduce the total number of cl to read
-            num_cl_image <= num_cl_image - 1;
-          end else if (num_cl_image_to_read == 0) begin
-            read_req_state <= TX_RD_STATE_KERNEL;
-            rd_req_en <= 1'b0;
-          end else begin
+          if (~rd_req_almostfull) begin
+            if (current_read_image_addr < filter_offset_addr && current_cycle_already_read_cl_image < NUM_CACHELINE_IMAGE_MOST) begin
+              rd_req_addr <= current_read_image_addr;
+              current_read_image_addr <= current_read_image_addr + 1;
+              rd_req_en <= 1'b1;
+              rd_rsp_mdata[0] <= 1'b0;   // 0 represents image
+              current_cycle_already_read_cl_image <= current_cycle_already_read_cl_image + 1;
+            end else begin
+              rd_req_en <= 1'b0;
+              current_cycle_already_read_cl_image <= 0;
+              current_read_filter_addr <= filter_offset_addr;
+              read_req_state <= TX_RD_STATE_KERNEL_0;
+            end
+          end else begin    // QPI read request almost full
             rd_req_en <= 1'b0;
           end
         end
-        TX_RD_STATE_KERNEL: begin
+        TX_RD_STATE_KERNEL_0: begin
           // TODO: filter_buffer_almost_full is to be set
-          if (~rd_req_almostfull && num_cl_filter_to_read != 0 && ~filter_buffer_almost_full) begin
-            rd_req_addr <= current_read_filter_addr;
-            current_read_filter_addr <= current_read_filter_addr + 1;
-            rd_req_en <= 1'b1;
-            rd_rsp_mdata[0] <= 1'b1;  // 1 represents kernel
-            num_cl_filter_to_read <= num_cl_filter_to_read - 1;
-          end else if (num_cl_filter_to_read == 0) begin
+          if (~rd_req_almostfull) begin
+            
+          end else begin  // QPI read request almost full
             rd_req_en <= 1'b0;
-            if (num_cl_image == 0) begin
-              read_req_state <= TX_RD_STATE_DONE;
-            end else begin
-              read_req_state <= TX_RD_STATE_CONFIG;
-            end
+          end
+        end
+        TX_RD_STATE_KERNEL_1: begin
+          if (~rd_req_almostfull) begin
+
           end else begin
             rd_req_en <= 1'b0;
           end
@@ -338,7 +325,7 @@ module afu_user #(ADDR_LMT = 20, MDATA = 14, CACHE_WIDTH = 512) (
   end
 
 
-  // read response FSM
+  // read response FSM, forward data to FFT or kernel memory
   enum {RX_RD_STATE_IDLE, RX_RD_STATE_RUN} read_rsp_state;
   always@(posedge clk) begin 
     if (reset) begin
@@ -361,6 +348,7 @@ module afu_user #(ADDR_LMT = 20, MDATA = 14, CACHE_WIDTH = 512) (
   end
 
   // write request FSM
+
 
   // write response FSM (maybe used for synchronization)
 
